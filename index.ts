fc17677a3882a7b45598233790bd480cf0ebd50d
Sean Behan <codebam@riseup.net>
2023-06-26 18:46:52 -0400

    get ema from product candles

--- index.ts
+++ index.ts
@@ -84,7 +84,6 @@ export default {
 			(eth_price * 1 + 0.00012702).toFixed(5),
 			60 * 3
 		).then(console.log);
-		// only sell if the EMA is above the price by .
 		if (ema + 0.0001 < eth_price) {
 			const sell_btc = await newOrder(
 				"ETH-BTC",
